48b0b44986face824f2470aafe9a4f29145c5d84
Vaughn Bosu <vwbosu@gmail.com>
2023-12-16 15:01:26 -0800

    one man drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/OneDrive.java
@@ -0,0 +1,274 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.teleOp;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.driveTools.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.driveTools.StandardTrackingWheelLocalizer;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ * <p>
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ * <p>
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+// WHAT IS WHAT
+// gamepad.2 is "weapons" - claw (x- close, y- open) - slide (a- up, b- down)
+// gamepad.1 is driving - motors dual-stick drive, right bumper for slow mode
+
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name = "OneDrive", group = "Linear Opmode")
+public class OneDrive extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // Drive Motors + Encoders
+    SampleMecanumDrive drive = null;
+    StandardTrackingWheelLocalizer encoders = null;
+
+    // Linear Slide
+    private DcMotor smallGear = null;
+    private DcMotor bigGear = null;
+
+    double bigGearPower;
+    double smallGearPower;
+
+    int operationMode = 2;
+
+    private Servo Servo1 = null;
+
+    private Servo Servo2 = null;
+    // Used for vert linear slide
+    private boolean upperBoundHit = false;
+    private boolean lowerBoundHit = false;
+    // MAX_TICKS is the value at the top (don't raise up more than this)
+    // MIN_TICKS is the value at the bottom (don't wind up more than this)
+    final int MAX_TICKS = 3000;
+    final int MIN_TICKS = 0;
+
+    @Override
+    public void runOpMode() {
+
+        // Method to assign and initialize the hardware
+        initializeHardware();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            driveCode(gamepad1);
+            controlClaw(gamepad1);
+            controlLinearSlide(gamepad1);
+
+
+        }
+
+        Pose2d poseEstimate = drive.getPoseEstimate();
+        telemetry.addData("x", poseEstimate.getX());
+        telemetry.addData("y", poseEstimate.getY());
+        telemetry.addData("heading", poseEstimate.getHeading());
+    }
+
+    public void initializeHardware() {
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        drive = new SampleMecanumDrive(hardwareMap);
+        Servo1 = hardwareMap.get(Servo.class, "servo1");
+        Servo2 = hardwareMap.get(Servo.class, "servo2");
+
+
+        // Setting the motor encoder position to zero
+        drive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        // Ensuring the motors get the instructions
+        sleep(100);
+
+        // This makes sure the motors are not using encoders (we don't use them)
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Encoders
+        encoders = new StandardTrackingWheelLocalizer(hardwareMap);
+
+        setMotorsBreakMode();
+
+        // Linear Slide
+        smallGear = hardwareMap.get(DcMotor.class, "smallGear");
+        bigGear = hardwareMap.get(DcMotor.class, "bigGear");
+
+        smallGear.setDirection(DcMotor.Direction.FORWARD);
+        bigGear.setDirection(DcMotor.Direction.REVERSE);
+
+        bigGear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // DO NOT DO THIS. IT BREAKS THE MOTOR. IF YOU NEED TO RESET THE ENCODER
+        bigGear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        smallGear.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // small doesn't use encoder this is for troubleshoot
+
+        bigGear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        smallGear.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    private void driveCode(Gamepad gp) {
+
+        // Drive Constants
+        double maxSpeed = 1;
+
+        // Input Calculations
+        float x = gp.left_stick_x;
+        float y = -gp.left_stick_y;
+
+        double lfp = Range.clip(x + y, -maxSpeed, maxSpeed);
+        double lbp = Range.clip(y - x, -maxSpeed, maxSpeed);
+
+        double rfp = Range.clip(y - x, -maxSpeed, maxSpeed);
+        double rbp = Range.clip(x + y, -maxSpeed, maxSpeed);
+
+
+        // Fast rotation
+        float rotSpeed;
+        if (gp.right_stick_button)
+            rotSpeed = 1;
+        else
+            rotSpeed = 2;
+
+
+        // Slow mode
+        boolean speedModeActive = gp.right_bumper;
+        double speedModifier;
+        if (speedModeActive) {
+            speedModifier = 1;
+            setMotorsBreakMode();
+        } else {
+            speedModifier = .4;
+            setMotorsFloatMode();
+        }
+
+
+        // Rotational offset code factoring in precalculated drive code
+        float rot = -gp.right_stick_x;
+
+        lfp = Range.clip(lfp - rot / rotSpeed, -1.0, 1.0);
+        lbp = Range.clip(lbp - rot / rotSpeed, -1.0, 1.0);
+
+        rfp = Range.clip(rfp + rot / rotSpeed, -1.0, 1.0);
+        rbp = Range.clip(rbp + rot / rotSpeed, -1.0, 1.0);
+
+        // Send calculated power to wheels
+        drive.setMotorPowers(lfp * speedModifier, lbp * speedModifier, rbp * speedModifier, rfp * speedModifier);
+
+        // Printing to console
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motor Power", "lf (%.2f), rb (%.2f), lb (%.2f), rf (%.2f)", lfp, rbp, lbp, rfp);
+        telemetry.addData("Speed Modifier", "Master (%.2f), Rotational (%.2f)", speedModifier, rotSpeed);
+        telemetry.update();
+    }
+
+
+    private void controlLinearSlide(Gamepad gp) {
+
+        if(gp.dpad_up){
+            bigGearPower = 0.5;
+        } else if(gp.dpad_down){
+            bigGearPower = -0.5;
+        } else {
+            bigGearPower = 0;
+        }
+
+        bigGear.setPower(bigGearPower);
+    }
+
+
+    public void controlClaw(Gamepad gp) {
+        if (gp.a) {
+            Servo1.setPosition(0); //
+        } else if (gp.b) {
+            Servo1.setPosition(0.19);
+        } else if (gp.x){
+            Servo1.setPosition(0.18);
+        }
+
+        if(gp.right_trigger > 0.25){
+            Servo2.setPosition(.55);
+        } else if(gp.left_trigger > 0.25){
+            Servo2.setPosition(.9);
+        }
+    }
+
+    public void setMotorsBreakMode() {
+        drive.setMotorsBreakMode();
+//        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void setMotorsFloatMode() {
+        drive.setMotorsFloatMode();
+//        rightBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+//        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+//        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+//        leftBackDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+    }
+
+    public void slideMotorsBrakeMode() {
+        bigGear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        smallGear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        telemetry.addData("Motors", "Brake Mode");
+    }
+    public void slideMotorsFloatMode() {
+        bigGear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        smallGear.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        telemetry.addData("Motors", "Float Mode");
+    }
+
+}
